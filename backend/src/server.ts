b3ed37efa56c8ee2c7369b9db24839ce08c2eb2d
joanasoaresd <joana.darck@dcx.ufpb.br>
2020-10-17 04:24:34 -0300

    fazendo conexão com banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,35 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
 app.use(express.json());
 
-//ROTA = CONJUNTO
-// RECURSO = USUARIO
-
-// METODOS HTTP = GET, POST, PUT, DELETE
-//PARAMETROS
-
-// GET BUSCAR UMA INFORMAÇÃO
-// POST CRIANDO UMA INFORMAÇÃO NOVA
-// PUT EDITANDO UMA INFORMAÇÃO
-// DELETE DELETAR UMA INFORMAÇÃO
-
-// Query Params: http://localhost:3333/users?search=joana
-// Route Params: http://localhost:3333/users/1 (indentificar um recurso)
-// Body:  http://localhost:3333/users
-
-app.post('/users/:id', (request, response) => {
-
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
-
-    return response.json({ message: 'Hello World'});
-    
+app.get('/users/:id', (request, response) => {
+    return response.json({ message: 'Hello World'});    
 });
 
 app.listen(3333);
-
-// REQ / RES
-// localhost: 3333
