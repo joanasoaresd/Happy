cf12801b79dc15e5adb344056532e062a9d7fc32
joanasoaresd <joana.darck@dcx.ufpb.br>
2020-10-17 04:04:50 -0300

    entendendo rotas, recursos, metodos http e paramentros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,34 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333);
+app.use(express.json());
 
-app.get('/users', (request, response) => {
+//ROTA = CONJUNTO
+// RECURSO = USUARIO
+
+// METODOS HTTP = GET, POST, PUT, DELETE
+//PARAMETROS
+
+// GET BUSCAR UMA INFORMAÇÃO
+// POST CRIANDO UMA INFORMAÇÃO NOVA
+// PUT EDITANDO UMA INFORMAÇÃO
+// DELETE DELETAR UMA INFORMAÇÃO
+
+// Query Params: http://localhost:3333/users?search=joana
+// Route Params: http://localhost:3333/users/1 (indentificar um recurso)
+// Body:  http://localhost:3333/users
+
+app.post('/users/:id', (request, response) => {
+
+    console.log(request.query);
+    console.log(request.params);
+    console.log(request.body);
+
     return response.json({ message: 'Hello World'});
     
-} )
+});
+
+app.listen(3333);
 
 // REQ / RES
 // localhost: 3333
